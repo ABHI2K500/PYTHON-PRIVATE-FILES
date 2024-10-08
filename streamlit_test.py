fc12c06bd337f3b8109a6e75f6d37a4fd728cfd9
ABHI2K500 <abhijithp2028@cs.sjcetpalai.ac.in>
2024-10-08 16:26:29 +0530

    Add files via upload

---
+++ streamlit_test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.button("I AM BATMAN",type='primary')
+st.button('TEST 1',type='secondary')
+left, middle, right = st.columns(3)
+if left.button("Plain button", use_container_width=True):
+    left.markdown("You clicked the plain button.")
+if middle.button("Emoji button", icon="😃", use_container_width=True):
+    middle.markdown("You clicked the emoji button.")
+if right.button("Material button", icon=":material/mood:", use_container_width=True):
+    right.markdown("You clicked the Material button.")
